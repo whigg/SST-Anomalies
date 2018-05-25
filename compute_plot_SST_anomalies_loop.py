056f8e56acbfb112a35f9f96a9c12f87424df553
ctroupin <charles.troupin@gmail.com>
2018-05-25 09:44:02 +0200

    add logging and change colormap

--- compute_plot_SST_anomalies_loop.py
+++ compute_plot_SST_anomalies_loop.py
@@ -1,4 +1,3 @@
-import netCDF4
 import sstOceanColor
 import os
 import sys
@@ -6,17 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from matplotlib import rcParams
-from matplotlib import rc
-import datetime
 import calendar
 import logging
 import warnings
+import cmocean
 import matplotlib.cbook
 warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
-
-
-# The data files in netCDF format are obtained from https://oceandata.sci.gsfc.nasa.gov/.<br>
-# The plots are created using the 9 km resolution file, which are sufficient for our purpose.
 
 # Configuration
 logging.basicConfig(format='%(asctime)s | %(levelname)s : %(message)s',
@@ -28,9 +22,9 @@ logger.setLevel(logging.DEBUG)
 rcParams.update({'font.size': 22})
 
 # Period and directories
-year = 2017
-monthlist = [1, 2, 3, 4, 5, 6, 7, 8]
-# monthlist = [1]
+year = 2018
+# monthlist = [1, 2, 3, 4, 5, 6, 7, 8]
+monthlist = [1, 2, 3]
 satellite = 'Terra'
 datamonthdir = "/home/ctroupin/Data/SST/Global/monthly/"
 dataclimdir = "/home/ctroupin/Data/SST/Global/monthly_clim/"
@@ -38,20 +32,21 @@ figdir = ["/home/ctroupin/Data/SST/figures/MedSea",
           "/home/ctroupin/Data/SST/figures/Canary",
           "/home/ctroupin/Data/SST/figures/Global"]
 
-# Domains
+# Domains: MedSea and North Atlantic
 domainlist = [(-7.0, 36.0, 30.0, 47.0, 8., 4.),
               (-80, 0, 0., 40., 15., 10.)]
 
-
 # Create the projections
 m = []
+i = 0
 for domain in domainlist:
+    i += 1
+    logging.info("Preparing basemap for domain: {}".format(i))
     m.append(Basemap(projection='merc', llcrnrlon=domain[0], llcrnrlat=domain[2],
                      urcrnrlon=domain[1], urcrnrlat=domain[3], lat_ts=0.5*(domain[0] + domain[2]),
                      resolution='h'))
 
 mg = Basemap(projection='ortho', lon_0=-15, lat_0=20, resolution='i')
-
 
 for month in monthlist:
     logger.info("Working on month {0}".format(month))
@@ -60,67 +55,78 @@ for month in monthlist:
     MonthlyFileName = sstOceanColor.makemonthfilename(year, month, satellite)
     MonthlyClimFileName = sstOceanColor.makeclimfilename(year, month, satellite)
 
+    logger.info("Monthly file: {}".format(MonthlyFileName))
+    logger.info("Monthly climatology file: {}".format(MonthlyClimFileName))
+
     # Create figure names
     fignameregion = 'SST_anomaly_{0}_{1}_{2}'.format(satellite, year, month)
+    fignameregionfield = 'SST_{0}_{1}_{2}'.format(satellite, year, month)
     fignameglob = 'SST_anomaly_global_{0}_{1}_{2}'.format(satellite, year, month)
+    fignameglobfield = 'SST_global_{0}_{1}_{2}'.format(satellite, year, month)
     logger.info(fignameregion)
 
     # Read SST
-    with netCDF4.Dataset(os.path.join(datamonthdir, MonthlyFileName)) as nc:
-        sstvar = nc.get_variables_by_attributes(standard_name="sea_surface_temperature")[0]
-        sstvarname = sstvar.name
-        lon = nc.variables['lon'][:]
-        lat = nc.variables['lat'][:]
-        sst = nc.variables[sstvarname][:]
-
-    with netCDF4.Dataset(os.path.join(dataclimdir, MonthlyClimFileName)) as nc:
-        sstclim = nc.variables[sstvarname][:]
+    # (whole field, so that we can loop and perform extractions)
+    sstmonth = sstOceanColor.SSTfield().readfile(os.path.join(datamonthdir, MonthlyFileName))
+    sstclim = sstOceanColor.SSTfield().readfile(os.path.join(dataclimdir, MonthlyClimFileName))
 
     # Compute anomalies
-    sstanom = sst - sstclim
-    logger.debug(sstanom.shape)
+    sstanom = sstOceanColor.SSTfield()
+    sstanom.lon = sstmonth.lon
+    sstanom.lat = sstmonth.lat
+    sstanom.sst = sstmonth.sst - sstclim.sst
+    logger.debug(sstanom.sst.shape)
 
-    figtitle = "SST anomalies ($^\circ$C) $-$ Satellite {0} \n{1} {2}".format(satellite,
-                                                                                  calendar.month_name[month],
-                                                                                  str(year))
+    figtitleanom = "SST anomalies ($^\circ$C) $-$ Satellite {0} \n{1} {2}".format(satellite,
+                                                                              calendar.month_name[month],
+                                                                              str(year))
+    figtitlefield = "SST ($^\circ$C) $-$ Satellite {0} \n{1} {2}".format(satellite,
+                                                                             calendar.month_name[month],
+                                                                             str(year))
+
     # Loop on domains
     for ii, domain in enumerate(domainlist):
-        goodlon = np.where((lon >= domain[0]) & (lon <= domain[1]))[0]
-        goodlat = np.where((lat >= domain[2]) & (lat <= domain[3]))[0]
-        lond = lon[goodlon]
-        latd = lat[goodlat]
-        sstanomplot = sstanom[goodlat, :]
-        sstanomplot = sstanomplot[:, goodlon]
 
-        llon, llat = np.meshgrid(lond, latd)
-        lonp, latp = m[ii](llon, llat)
+        # Plot SST field
+        sstmonth.extractdomain(domain)
+        sstmonth.plotfield(m[ii], domain, figtitlefield)
+        plt.savefig(os.path.join(figdir[ii], "".join((fignameregionfield, '_', str(ii), ".png"))),
+                    dpi=300, bbox_inches='tight')
 
-        print(llon.shape)
 
-        plt.figure(figsize=(10, 8))
-        pcm = m[ii].pcolormesh(lonp, latp, sstanomplot, cmap=plt.cm.RdBu_r, vmin=-3, vmax=3)
-        plt.colorbar(pcm, extend='both', orientation='horizontal', shrink=0.95, pad=0.06)
-        plt.title(figtitle)
-        m[ii].drawcoastlines(linewidth=0.2)
-        m[ii].fillcontinents(color=".2")
-        m[ii].drawmeridians(np.arange(domain[0], domain[1], domain[-2]), labels=[0, 0, 0, 1], linewidth=0.2)
-        m[ii].drawparallels(np.arange(domain[2], domain[3], domain[-1]), labels=[1, 0, 0, 0], linewidth=0.2)
-        plt.savefig(os.path.join(figdir[ii], "".join((fignameregion, '_', str(ii), ".png"))), dpi=300, bbox_inches='tight')
+        # Plot anomalies
+        sstanom.extractdomain(domain)
+        sstanom.plotanom(m[ii], domain, figtitleanom)
+        plt.savefig(os.path.join(figdir[ii], "".join((fignameregion, '_', str(ii), ".png"))),
+                    dpi=300, bbox_inches='tight')
         plt.close()
 
-    # Global map
 
-    llon, llat = np.meshgrid(lon, lat)
+    # Global map
+    # (no domain extraction)
+    llon, llat = np.meshgrid(sstanom.lon, sstanom.lat)
     long, latg = mg(llon, llat)
     long[long == long.max()] = np.nan
     latg[latg == latg.max()] = np.nan
 
+    # Field
     plt.figure(figsize=(10, 10))
-    pcm = mg.pcolormesh(long, latg, sstanom, cmap=plt.cm.RdBu_r, vmin=-3, vmax=3)
+    pcm = mg.pcolormesh(long, latg, sstmonth.sst, cmap=cmocean.cm.thermal, vmin=0., vmax=30.)
     plt.colorbar(pcm, extend='both', orientation='horizontal', shrink=0.85, pad=0.02)
-    plt.title(figtitle)
+    plt.title(figtitlefield)
+    mg.drawcoastlines(linewidth=0.2)
+    mg.fillcontinents(color=".2")
+    plt.savefig(os.path.join(figdir[-1], fignameglobfield), dpi=300, bbox_inches='tight')
+    plt.close()
+
+
+    # Anomalies
+    plt.figure(figsize=(10, 10))
+    pcm = mg.pcolormesh(long, latg, sstanom.sst, cmap=plt.cm.RdBu_r, vmin=-3, vmax=3)
+    plt.colorbar(pcm, extend='both', orientation='horizontal', shrink=0.85, pad=0.02)
+    plt.title(figtitleanom)
     mg.drawcoastlines(linewidth=0.2)
     mg.fillcontinents(color=".2")
     plt.savefig(os.path.join(figdir[-1], fignameglob), dpi=300, bbox_inches='tight')
-    plt.show()
+    # plt.show()
     plt.close()
